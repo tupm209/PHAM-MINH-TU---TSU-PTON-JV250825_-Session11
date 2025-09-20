24e384299beecad0e89fc0b8b25b08dbc7fad6ed
tupm209 <tu.pm209@gmail.com>
2025-09-21 02:05:13 +0900

    Update hw01.js

--- hw01/hw01.js
+++ hw01/hw01.js
@@ -5,7 +5,7 @@ while (num >0) {
 
   if ((numOfYear%4 === 0 && numOfYear%100 !== 0) || numOfYear%400 === 0) {
     alert("Đây là năm nhuận!");
-  } else if((numOfYear%100 === 0 && numOfYear%400 !== 0) || numOfYear%4 !== 0) {
+  } else {
     alert("Đây không phải là năm nhuận!");
   }
 }
