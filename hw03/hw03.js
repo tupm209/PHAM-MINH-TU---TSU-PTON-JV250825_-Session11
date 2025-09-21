519e350645a52364232badea547bb156755acf97
tupm209 <tu.pm209@gmail.com>
2025-09-22 02:13:13 +0900

    Add files via upload

---
+++ hw03/hw03.js
@@ -0,0 +1,37 @@
+let soNhap = Number(prompt("Hãy nhập một số bất kỳ từ 0 -> 9!"));
+
+switch (soNhap) {
+  case 0:
+    alert("Số không");
+    break;
+  case 1:
+    alert("Số một");
+    break;
+  case 2:
+    alert("Số hai");
+    break;
+  case 3:
+    alert("Số ba");
+    break;
+  case 4:
+    alert("Số bốn");
+    break;
+  case 5:
+    alert("Số năm");
+    break;
+  case 6:
+    alert("Số sáu");
+    break;
+  case 7:
+    alert("Số bảy");
+    break;
+  case 8:
+    alert("Số tám");
+    break;
+  case 9:
+    alert("Số chín");
+    break;
+
+  default:
+    break;
+}
